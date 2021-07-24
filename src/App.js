c6c62c7ec3aa167694256aa686f2c44df06a79fd
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-07-24 20:58:26 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,26 +3,14 @@ import React from 'react';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test my first react project !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {/* <Greet />
+      <Welcome /> */}
+      <Hello/>
     </div>
   );
 }
