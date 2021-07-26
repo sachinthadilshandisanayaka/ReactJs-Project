5ce5b7e5b86b4d88f35399420433b7b28d91ce65
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-07-27 00:03:05 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Sanju">
+      <Message />
+      {/* <Greet name="Sanju">
         <p>My age is 23</p>
       </Greet>
       <Greet name="Dilshan" />
-      <Welcome name="Kamal" />
+      <Welcome name="Kamal" /> */}
       {/* <Hello/> */}
     </div>
   );
---
+++ src/components/Message.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Message extends Component {
+    constructor() {
+        super()
+        this.state = {
+            message: 'Welcome Visitors'
+        }
+    }
+
+    changeMessage() {
+        this.setState({
+            message: 'Thank you for subscribing'
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> {this.state.message} </h1>
+                <button onClick={() => this.changeMessage()}>Subscribe</button>
+            </div>
+        );
+    }
+}
+export default Message;
