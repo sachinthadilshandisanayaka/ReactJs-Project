8852c4f3f00b7eec13b5e1a27b175593bf29397d
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-08-02 17:58:02 +0530

    updated

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,8 +2,34 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
-    const persons = ['Sachintha', 'Dilshan', 'Sanju', 'Malsha']
-    const personList = persons.map(person => <Person person={person} />)
+    const persons = [
+        {
+            id: '1',
+            name: 'sanju',
+            age: '12'
+        },
+        {
+            id: '2',
+            name: 'sachintha',
+            age: '13'
+        },
+        {
+            id: '3',
+            name: 'sanju 2',
+            age: '29'
+        },
+        {
+            id: '4',
+            name: 'sanju 3',
+            age: '20'
+        },
+        {
+            id: '5',
+            name: 'sanju 4',
+            age: '32'
+        }
+    ]
+    const personList = persons.map(person => <Person key={person.id} person={person} />)
     return (
         <div>{personList}</div>
     )
--- src/components/Person.js
+++ src/components/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Person({ person }) {
     return (
         <div>
-            <h2>{person}</h2>
+            <h2>Hello {person.name}, your age is {person.age} </h2>
         </div>
     )
 }
